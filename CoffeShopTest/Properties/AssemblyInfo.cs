fa4e6eb67b12dbe19fb06813e5550b7d1995709a
bnowacki <bknowacki@gmail.com>
2024-04-23 15:07:42 +0200

    data layer tests

--- CoffeShopTest/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("fe1a38b0-396b-4444-a580-54e821bc1f4e")]
